440722cc7635af345617148f16411e9271020f4c
MichaelShawUK <MichaelShawUK@gmail.com>
2024-07-07 17:54:49 +0100

    Define model relationships

--- app/Models/Plugin.php
+++ app/Models/Plugin.php
@@ -4,8 +4,25 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Plugin extends Model
 {
     use HasFactory;
+
+    public function author(): BelongsTo
+    {
+        return $this->belongsTo(Author::class);
+    }
+
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function tags(): BelongsToMany
+    {
+        return $this->belongsToMany(Tag::class);
+    }
 }
--- database/migrations/2024_07_07_160310_create_plugin_tag_table.php
+++ database/migrations/2024_07_07_160310_create_plugin_tag_table.php
@@ -15,8 +15,8 @@ return new class extends Migration
     {
         Schema::create('plugin_tag', function (Blueprint $table) {
             $table->id();
-            $table->foreignIdFor(Plugin::class);
-            $table->foreignIdFor(Tag::class);
+            $table->foreignIdFor(Plugin::class)->constrained()->cascadeOnDelete();
+            $table->foreignIdFor(Tag::class)->constrained()->cascadeOnDelete();
         });
     }
 
