e3cca3cb90916b31c88da420d4c83e1a728701e2
MichaelShawUK <MichaelShawUK@gmail.com>
2024-07-07 17:05:51 +0100

    Make table migrations

---
+++ database/migrations/2024_07_07_134241_create_plugins_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use App\Models\Author;
+use App\Models\Category;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('plugins', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->foreignIdFor(Author::class);
+            $table->text('description');
+            $table->integer('stars');
+            $table->string('url');
+            $table->foreignIdFor(Category::class);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('plugins');
+    }
+};
