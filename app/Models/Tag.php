a6f01586a42e84209dd348cf057f45819de20c89
MichaelShawUK <MichaelShawUK@gmail.com>
2024-09-07 18:35:47 +0100

    Mutate tag name to slug

--- app/Models/Tag.php
+++ app/Models/Tag.php
@@ -2,9 +2,11 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Support\Str;
 
 class Tag extends Model
 {
@@ -13,6 +15,13 @@ class Tag extends Model
     protected $fillable = [
         'name',
     ];
+
+    protected function name(): Attribute
+    {
+        return Attribute::make(
+            set: fn (string $value) => Str::of($value)->slug(),
+        );
+    }
 
     public function plugins(): BelongsToMany
     {
