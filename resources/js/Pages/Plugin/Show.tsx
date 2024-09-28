4e684ea2884f1d8b44c9dde30607a7d27a84908a
MichaelShawUK <MichaelShawUK@gmail.com>
2024-09-28 22:31:28 +0100

    Add CommentForm component

--- resources/js/Pages/Plugin/Show.tsx
+++ resources/js/Pages/Plugin/Show.tsx
@@ -1,8 +1,10 @@
 import PluginComment from '@/Components/PluginComment';
+import CommentForm from '@/Components/CommentForm';
 
 export default function Show() {
     return (
         <div>
+            <CommentForm />
             <PluginComment />
         </div>
     );
